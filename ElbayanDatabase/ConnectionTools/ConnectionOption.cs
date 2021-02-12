d8677afe03701386ed365a215009cc66b85b936d
Mohamed Ali <m.alii60@icloud.com>
2021-02-12 16:05:06 +0200

    addFilesAndCreateConnectionStringClasses

---
+++ ElbayanDatabase/ConnectionTools/ConnectionOption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayanDatabase.ConnectionTools
+{
+   public class ConnectionOption : DbContext
+    {
+        public ConnectionOption()
+            : base(ConnectionString.Create())
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
