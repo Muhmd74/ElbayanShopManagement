8656425001453eee024b220a230180ee9f75fa6f
Mohamed Ali <m.alii60@icloud.com>
2021-02-12 19:08:08 +0200

    Create Category And SubCategory

---
+++ ElbayanDatabase/DataClasses/Product/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Migrations.Model;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElbayanDatabase.DataClasses.Product.ProductCategory;
+using ElbayanDatabase.DataClasses.Product.Unit;
+
+namespace ElbayanDatabase.DataClasses.Product
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string UCP { get; set; }
+        public string BarCode { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal DefaultPrice { get; set; }
+        public Guid SubCategoryId { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public long ProductNumber { get; set; }
+        public bool IsExpired { get; set; } = false;
+        public Guid LargeUnitId { get; set; }
+        public LargeUnit LargeUnit { get; set; }
+        public SmallUnit SmallUnit { get; set; }
+        public Guid SmallUnitId { get; set; }
+
+        public int LimitedDemand { get; set; }
+
+
+
+    }
+}
---
+++ ElbayanDatabase/DataClasses/Product/ProductCategory/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElbayanDatabase.DataClasses.Product.ProductCategory
+{
+   public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public ICollection<SubCategory> SubCategories { get; set; }
+
+    }
+}
