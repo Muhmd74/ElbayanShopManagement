6fe0e123ca465c569660bf0f6b80d3aece1d6fb1
Mohamed Mohamed Elhelbawi <m_elhelbawy@outlook.com>
2021-02-12 19:09:42 +0200

    Add Main Category Form

---
+++ ElbayanPresentation/Presenters/Category/IViewMainCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayanPresentation.Presenters.Category
+
+{
+    public interface IViewMainCategory
+    {
+        string CategoryName { get; set; }
+        string CategoryDescription { get; set; }
+        MainCategoryPresenter Presenter { set; }
+
+    }
+}
