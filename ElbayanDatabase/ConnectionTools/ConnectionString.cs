a1eb68ff9791acadd28e390ade40e69eccb09245
Mohamed Ali <m.alii60@icloud.com>
2021-02-12 16:05:06 +0200

    addFilesAndCreateConnectionStringClasses

---
+++ ElbayanDatabase/ConnectionTools/ConnectionString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayanDatabase.ConnectionTools
+{
+  public  class ConnectionString
+    {
+        public static string Create()
+        {
+            return DevMachine();
+        }
+        private static string DevMachine()
+        {
+            var buillder = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = "ElBayan",
+                IntegratedSecurity = true,
+                DataSource = @".\SQLEXPRESS",
+                ApplicationName = "Elbayan"
+            };
+            return buillder.ConnectionString;
+
+        }
+    }
+}
