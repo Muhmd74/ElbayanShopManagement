607ae492b53b7e74e6389c781b9f16221d512afb
Mohamed Mohamed Elhelbawi <m_elhelbawy@outlook.com>
2021-02-12 19:09:42 +0200

    Add Main Category Form

---
+++ ElbayanPresentation/Presenters/Category/MainCategoryPresenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayanPresentation.Presenters.Category
+{
+    public class MainCategoryPresenter
+    {
+        private readonly IViewMainCategory _view;
+
+        public MainCategoryPresenter(IViewMainCategory view)
+        {
+            _view = view;
+        }
+
+
+    }
+}
---
+++ ElbayanPresentation/Views/MainCategory.cs
@@ -0,0 +1,30 @@
+﻿using ElbayanPresentation.Presenters.Category;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElbayanPresentation.Views
+{
+    public partial class MainCategory : Form, IViewMainCategory
+    {
+        public MainCategory()
+        {
+            InitializeComponent();
+        }
+
+        public string CategoryName { get => txtCategoryName.Text; set => txtCategoryName.Text = value; }
+        public string CategoryDescription { get => txtCategoryDescription.Text; set => txtCategoryDescription.Text = value; }
+        public MainCategoryPresenter Presenter { private get; set; }
+
+        private void btnAddCategory_Click(object sender, EventArgs e)
+        {
+            Presenter = new MainCategoryPresenter(this);
+        }
+    }
+}
