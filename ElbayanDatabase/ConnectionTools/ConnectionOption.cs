c81158567461007278fdf97776f6f415e19c520c
Mohamed Ali <m.alii60@icloud.com>
2021-02-12 19:08:08 +0200

    Create Category And SubCategory

--- ElbayanDatabase/ConnectionTools/ConnectionOption.cs
+++ ElbayanDatabase/ConnectionTools/ConnectionOption.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElbayanDatabase.DataClasses.Member;
+using ElbayanDatabase.DataClasses.Product;
+using ElbayanDatabase.DataClasses.Product.ProductCategory;
+using ElbayanDatabase.DataClasses.Product.Unit;
 
 namespace ElbayanDatabase.ConnectionTools
 {
@@ -13,12 +18,56 @@ namespace ElbayanDatabase.ConnectionTools
         public ConnectionOption()
             : base(ConnectionString.Create())
         {
-            
+          
         }
-
+         
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<MemberRole>  MemberRoles { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<LargeUnit> LargeUnits { get; set; }
+        public DbSet<SmallUnit> SmallUnits { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Role>()
+                .HasMany(d=>d.MemberRoles)
+                .WithRequired(d=>d.Role)
+                .HasForeignKey(d=>d.RoleId)
+                .WillCascadeOnDelete(true);
+            modelBuilder.Entity<Member>()
+                .HasMany(d=>d.MemberRoles)
+                .WithRequired(d=>d.Member)
+                .HasForeignKey(d=>d.MemberId)
+                .WillCascadeOnDelete(true);
+            //Product
+            modelBuilder.Entity<Category>()
+                .HasMany(d=>d.SubCategories)
+                .WithRequired(d=>d.Category)
+                .HasForeignKey(d=>d.CategoryId)
+                .WillCascadeOnDelete(true);
+            modelBuilder.Entity<SubCategory>()
+                .HasMany(d=>d.Products)
+                .WithRequired(d=>d.SubCategory)
+                .HasForeignKey(d=>d.SubCategoryId)
+                .WillCascadeOnDelete(true);
+            modelBuilder.Entity<LargeUnit>()
+                .HasMany(d=>d.Products)
+                .WithRequired(d=>d.LargeUnit)
+                .HasForeignKey(d=>d.LargeUnitId)
+                .WillCascadeOnDelete(true);
+            modelBuilder.Entity<SmallUnit>()
+                .HasMany(d=>d.Products)
+                .WithRequired(d=>d.SmallUnit)
+                .HasForeignKey(d=>d.SmallUnitId)
+                .WillCascadeOnDelete(true);
+            modelBuilder.Entity<Category>()
+                .Property(d => d.Id)
+                .Equals(Guid.NewGuid());
+
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ ElbayanServices/Repository/Products/SubCategory/SubCategoryService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ElbayanDatabase.ConnectionTools;
+using ElbayanServices.Repository.Products.SubCategory.Dtos;
+
+namespace ElbayanServices.Repository.Products.SubCategory
+{
+   public class SubCategoryService : ISubCategory
+   {
+       private readonly ConnectionOption _context;
+
+       public SubCategoryService(ConnectionOption context)
+       {
+           _context = context;
+       }
+
+       public SubCategoryDto Add(SubCategoryDto model)
+       {
+           try
+           {
+               var result = _context.SubCategories.Add(new ElbayanDatabase.DataClasses.Product.ProductCategory.SubCategory
+               {
+                   Name = model.Name,
+                   Description = model.Description,
+                   CategoryId = model.CategoryId
+
+               });
+               _context.SaveChanges();
+               return new SubCategoryDto()
+               {
+                   Name = model.Name,
+                   Description = model.Description,
+                   Id = result.Id
+               };
+           }
+           catch (Exception e)
+           {
+              return null;
+           }
+       }
+
+        public SubCategoryDto Update(SubCategoryDto model)
+        {
+            var result = _context.SubCategories.FirstOrDefault(d => d.Id == model.Id);
+            if (result!=null)
+            {
+                result.Description = model.Description;
+                result.Name = model.Name;
+                result.CategoryId = model.CategoryId;
+                _context.SaveChanges();
+                return new SubCategoryDto()
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Id = result.Id
+                };
+            }
+
+            return null;
+        }
+
+        public bool Delete(Guid id)
+        {
+            var result = _context.SubCategories.FirstOrDefault(d => d.Id == id);
+            if (result!=null)
+            {
+                _context.SubCategories.Remove(result);
+                _context.SaveChanges();
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public List<SubCategoryDto> GetAll()
+        {
+            var model = _context.SubCategories
+                .Select(d => new SubCategoryDto()
+            {
+                Name = d.Name,
+                Description = d.Description,
+                CategoryId = d.CategoryId,
+                CategoryName = d.Category.Name
+            }).ToList();
+            return model.Any() ? model : null;
+        }
+
+        public SubCategoryDto GetById(Guid id)
+        {
+            var result = _context.SubCategories.FirstOrDefault(d => d.Id == id);
+            if (result!=null)
+            {
+                return new SubCategoryDto()
+                {
+                    Name = result.Name,
+                    Description = result.Description,
+                    CategoryId = result.CategoryId,
+                    CategoryName = result.Category.Name,
+                    Id = result.Id
+                };
+            }
+
+            return null;                                                                                   
+        }
+    }
+}
