8ad65a87aca69c189ab79071b987b56adfd58236
Mohamed Mohamed Elhelbawi <m_elhelbawy@outlook.com>
2021-02-14 17:03:15 +0200

    Add MAinForm, Struct presentation project

---
+++ ElbayanPresentation/frmElbayanMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElbayanPresentation
+{
+    public partial class frmElbayanMain : Form
+    {
+        public frmElbayanMain()
+        {
+            InitializeComponent();
+            // StartPosition was set to FormStartPosition.Manual in the properties window.
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            int w = Screen.PrimaryScreen.WorkingArea.Width;
+            int h = Screen.PrimaryScreen.WorkingArea.Width;
+            this.Location = new Point(0,0);
+            this.Size = new Size(w, h);
+        }
+
+        private void frmElbayanMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
