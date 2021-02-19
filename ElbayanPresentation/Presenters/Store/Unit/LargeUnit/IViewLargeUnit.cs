24a9b991e938748bb14407fa1cfb505134a6d75f
Mohamed Mohamed Elhelbawi <m_elhelbawy@outlook.com>
2021-02-19 23:03:00 +0200

    Create IViewLargeUnit, Create LargeUInt Presenter,

---
+++ ElbayanPresentation/Presenters/Store/Unit/LargeUnit/IViewLargeUnit.cs
@@ -0,0 +1,21 @@
+﻿using ElbayanServices.Repository.Products.Units.LargeUnit.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayaNPresentation.Presenters.Store.Unit.LargeUnit
+{
+    public interface IViewLargeUnit
+    {
+        string LargeUnitName { get; set; }
+        string LargeUnitDescirption { get; set; }
+
+        string SearchKeyword { get; set; }
+
+        List<LargeUnitDto> LargeUnit { get; set; }
+
+        LargeUnitPresenter Presenter { set; }
+    }
+}
---
+++ ElbayanPresentation/Presenters/Store/Unit/LargeUnit/LargeUnitPresenter.cs
@@ -0,0 +1,31 @@
+﻿using ElbayanServices.Repository.Products.Units.LargeUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayaNPresentation.Presenters.Store.Unit.LargeUnit
+{
+    public class LargeUnitPresenter
+    {
+        private readonly IViewLargeUnit _view;
+        private readonly LargeUnitService largeUnit = new LargeUnitService(new ElbayanDatabase.ConnectionTools.ConnectionOption());
+
+
+        public LargeUnitPresenter(IViewLargeUnit view)
+        {
+            _view = view;
+        }
+
+        public void AddNewUnit()
+        {
+            largeUnit.Add(new ElbayanServices.Repository.Products.Units.LargeUnit.Dtos.LargeUnitDto
+            {
+                Name = _view.LargeUnitName,
+                Description = _view.LargeUnitDescirption
+            });
+        }
+
+    }
+}
