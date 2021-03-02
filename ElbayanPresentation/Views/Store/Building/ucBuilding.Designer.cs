9656b7de9ebc16bede9e1c224d428892a3254cd0
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-03 00:10:17 +0200

    Add uc Builing, struc Buildimg and POSs

---
+++ ElbayanPresentation/Views/Store/Building/ucBuilding.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ElbayaNPresentation.Views.Store.Building
+{
+    partial class ucBuilding
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucBuilding
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Font = new System.Drawing.Font("Sakkal Majalla", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Appearance.Options.UseBackColor = true;
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "ucBuilding";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(1550, 762);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
