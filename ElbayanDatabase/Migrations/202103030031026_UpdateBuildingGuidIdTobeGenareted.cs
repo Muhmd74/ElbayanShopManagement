c334990f5e6e4fb5a8f7ed33d6a8559c6c2478f6
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-03 10:16:51 +0200

    Conflict bug

---
+++ ElbayanDatabase/Migrations/202103030031026_UpdateBuildingGuidIdTobeGenareted.cs
@@ -0,0 +1,26 @@
+﻿namespace ElbayanDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateBuildingGuidIdTobeGenareted : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.POS", "BuildingId", "dbo.Buildings");
+            DropPrimaryKey("dbo.Buildings");
+            AlterColumn("dbo.Buildings", "Id", c => c.Guid(nullable: false, identity: true, defaultValueSql: "newsequentialid()"));
+            AddPrimaryKey("dbo.Buildings", "Id");
+            AddForeignKey("dbo.POS", "BuildingId", "dbo.Buildings", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.POS", "BuildingId", "dbo.Buildings");
+            DropPrimaryKey("dbo.Buildings");
+            AlterColumn("dbo.Buildings", "Id", c => c.Guid(nullable: false));
+            AddPrimaryKey("dbo.Buildings", "Id");
+            AddForeignKey("dbo.POS", "BuildingId", "dbo.Buildings", "Id", cascadeDelete: true);
+        }
+    }
+}
