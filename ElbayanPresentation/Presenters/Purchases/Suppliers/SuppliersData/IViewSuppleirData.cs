9dafa63bce0abd201be883ed3e1a16b50858f89d
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-05 22:54:13 +0200

    Struct Suppliers Data to CRUD Suppliers

---
+++ ElbayanPresentation/Presenters/Purchases/Suppliers/SuppliersData/IViewSuppleirData.cs
@@ -0,0 +1,32 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElbayaNPresentation.Presenters.Purchases.Suppliers.SuplliersData
+{
+    public interface IViewSuppleirData
+    {
+        Guna2TextBox SuppliersName { get; set; }
+        Guna2TextBox SuppleirIdNumber { get; set; }
+        Guna2TextBox FirmName { get; set; }
+        Guna2TextBox Address { get; set; }
+        Guna2TextBox CommercialRegister { get; set; }
+        Guna2TextBox SupplierNumber { get; set; }
+        Guna2TextBox Description  { get; set; }
+        Guna2ComboBox CustomerType { get; set; }
+        Guna2TextBox Mobile { get; set; }
+        Guna2TextBox TaxNumber { get; set; }
+        NumericUpDown OpeningBalance { get; set; }
+
+        DataGrid ActiveObjetc { get; set; }
+        DataGrid DeletedObject { get; set; }
+
+        PresenterSupplierData Presenter { get; set; }
+
+
+    }
+}
---
+++ ElbayanPresentation/Presenters/Purchases/Suppliers/SuppliersData/PresenterSupplierData.cs
@@ -0,0 +1,20 @@
+﻿using ElbayanServices.Repository.Customers.Suppliers.Supplier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayaNPresentation.Presenters.Purchases.Suppliers.SuplliersData
+{
+    public class PresenterSupplierData
+    {
+        private readonly IViewSuppleirData _view;
+        private readonly SupplierService supplierService = new SupplierService(new ElbayanDatabase.ConnectionTools.ConnectionOption());
+
+        public PresenterSupplierData(IViewSuppleirData view)
+        {
+            _view = view;
+        }
+    }
+}
