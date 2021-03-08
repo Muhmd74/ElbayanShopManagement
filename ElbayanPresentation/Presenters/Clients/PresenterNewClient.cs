37d50d0b76d50dc10c3fcb1f53a61ce9a7c1d7b0
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-08 18:42:03 +0200

    Implement Add new CLient

--- ElbayanPresentation/Presenters/Clients/PresenterNewClient.cs
+++ ElbayanPresentation/Presenters/Clients/PresenterNewClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElbayanServices.Repository.Clints.Customer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,37 @@ namespace ElbayaNPresentation.Presenters.Clients
     public class PresenterNewClient
     {
         private readonly IViewNewClient _view;
+        private readonly CustomerService Client = new CustomerService(new ElbayanDatabase.ConnectionTools.ConnectionOption());
 
         public PresenterNewClient(IViewNewClient view)
         {
             _view = view;
         }
+
+        // CRUD:
+        // 1. Create new Object:
+
+        public void OnClickbtnAddNewOpect()
+        {
+            Client.CreateCustomer(new ElbayanServices.Repository.Clints.Supplier.Dtos.ClintDto { 
+                Name = _view.SuppliersName.Text,
+                NationalIdentity = Convert.ToInt32(_view.IdentityNumber.Text),
+                IsActive = _view.IsActive.Checked,
+                FirmName = _view.FirmName.Text,
+                Address = _view.Address.Text,
+                CommercialRegister = _view.TradeLicense.Text,
+                ReferenceNumber = Convert.ToInt32(_view.RefSupplierNumber.Text),
+                Description = _view.Note.Text,
+                Mobile = _view.Mobile.Text,
+                TaxNumber = _view.TaxNumber.Text,
+                OpeningBalance = Convert.ToInt32( _view.OpeningBalance.Value),
+                FaxNumber = _view.Fax.Text,
+                IsSupplier = _view.IsSupplier,
+                IsCustomer = _view.IsCustomer,
+                Phone = _view.
+            });
+        }
+        
+
     }
 }
