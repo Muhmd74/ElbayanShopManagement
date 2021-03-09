d5a548445afe86aaceff3c13ef1a2e6a0c49bdea
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-09 16:13:47 +0200

    Implement Sengltone Pattern in frm Employee Details

--- ElbayanPresentation/Views/Finance/Employee/frmEmployeeDetails.Designer.cs
+++ ElbayanPresentation/Views/Finance/Employee/frmEmployeeDetails.Designer.cs
@@ -33,14 +33,17 @@ namespace ElbayaNPresentation.Views.Finance.Employee
             // 
             // frmEmployeeDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1254, 867);
             this.ControlBox = false;
+            this.Font = new System.Drawing.Font("Sakkal Majalla", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmEmployeeDetails";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
-            this.Text = "frmEmployeeDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "بيانات الموظفين العاملين في الشركة";
             this.ResumeLayout(false);
 
         }
--- ElbayanPresentation/Views/Finance/Employee/frmEmployeeDetails.cs
+++ ElbayanPresentation/Views/Finance/Employee/frmEmployeeDetails.cs
@@ -15,6 +15,17 @@ namespace ElbayaNPresentation.Views.Finance.Employee
         public frmEmployeeDetails()
         {
             InitializeComponent();
+            _instance = this;
+        }
+        private static frmEmployeeDetails _instance;
+        public static frmEmployeeDetails Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new frmEmployeeDetails();
+                return _instance;
+            }
         }
     }
 }
