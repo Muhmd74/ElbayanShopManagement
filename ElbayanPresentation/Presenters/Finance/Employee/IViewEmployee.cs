6a36d92b5f6feeaee72c1c8cd99f805a48616fff
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-09 16:06:41 +0200

    Add IViewEmployee

--- ElbayanPresentation/Presenters/Finance/Employee/IViewEmployee.cs
+++ ElbayanPresentation/Presenters/Finance/Employee/IViewEmployee.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ElbayaNPresentation.Presenters.Finance.Employee
 {
     public interface IViewEmployee
     {
-        
+        PresenterEmployee Presenter { get; set; }
+        Guid ID { get; set; }
+        Guna2TextBox Name { get; set; }
+        Guna2TextBox Email { get; set; }
+        Guna2TextBox Password { get; set; }
+        Guna2TextBox Mobile { get; set; }
+        Guna2TextBox Address { get; set; }
+        Guna2TextBox Identity { get; set; }
+        DateTimePicker DateOfBirth { get; set; }
+        DateTimePicker IdentityExpirationDate { get; set; }
+        Guna2TextBox PassportNumber { get; set; } // int
+        DateTimePicker PassportExpirationDate { get; set; }
+        Guna2TextBox ResidenceType { get; set; }
+        Guna2TextBox Position { get; set; }
+        CheckBox IsActive { get; set; }
     }
 }
---
+++ ElbayanPresentation/Presenters/Finance/Employee/PresenterEmployee.cs
@@ -0,0 +1,19 @@
+﻿using ElbayanServices.Repository.Employees.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElbayaNPresentation.Presenters.Finance.Employee
+{
+    public class PresenterEmployee
+    {
+        private readonly IViewEmployee _view;
+        private readonly EmployeeService employeeService = new EmployeeService(new ElbayanDatabase.ConnectionTools.ConnectionOption());
+        public PresenterEmployee(IViewEmployee view)
+        {
+            _view = view;
+        }
+    }
+}
