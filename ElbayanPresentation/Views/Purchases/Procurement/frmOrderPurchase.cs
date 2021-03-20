a7f622e5242fbced28758b570ca30ef7c12b0753
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-03-21 01:06:45 +0200

    Order Purchase new form Design

---
+++ ElbayanPresentation/Views/Purchases/Procurement/frmOrderPurchase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElbayaNPresentation.Views.Purchases.Procurement
+{
+    public partial class frmOrderPurchase : MetroFramework.Forms.MetroForm
+    {
+        public frmOrderPurchase()
+        {
+            InitializeComponent();
+            _intsance = this;
+            txtBarcode.Select();
+        }
+        private static frmOrderPurchase _intsance;
+        public static frmOrderPurchase Intance
+        {
+            get
+            {
+                if (_intsance == null)
+                    _intsance = new frmOrderPurchase();
+                return _intsance;
+            }
+        }
+
+    }
+}
