d202b13ba8055af367e11d470d362bee78f91d47
Mohamed Ali <m.alii60@icloud.com>
2021-04-16 10:31:11 +0200

    Create Order return Suppliers

---
+++ ElbayanServices/Repository/Suppliers/OrderReturn/IOrderReturn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElbayanServices.Repository.Suppliers.OrderReturn.Dtos;
+
+namespace ElbayanServices.Repository.Suppliers.OrderReturn
+{
+   public interface IOrderReturn
+    {
+        Guid CreateSupplierOrderReturn(OrderReturnDto model);
+    }
+}
